50ea9374fbe239de5ab1cbc70d1afee4b9ecf295
Axl FC <afaces@salou.tinet.cat>
2026-04-10 17:55:34 +0200

    ADDED: First Commit

---
+++ dst-data/Caves/worldgenoverride.lua
@@ -0,0 +1,18 @@
+return {
+    override_enabled = true,
+    preset = "DST_CAVE",
+    overrides = {
+        world_size = "default",
+        branching = "default",
+        loop = "default",
+
+        wormattacks = "default",
+        earthquakes = "default",
+        tentacles = "default",
+
+        monkey = "default",
+        mushtree = "default",
+
+        fern = "often"
+    }
+}
---
+++ dst-data/dedicated_server_mods_setup.lua
@@ -0,0 +1,21 @@
+-- QoL / Información
+ServerModSetup("666155465")   -- Show Me (Origin)
+ServerModSetup("1207269058")  -- Simple Health Bar DST
+
+-- Comodidad / Construcción
+ServerModSetup("856487758")   -- Quick Drop
+ServerModSetup("501385076")   -- Quick Pick
+ServerModSetup("585654889")   -- Glowing Portal
+ServerModSetup("1416161108")  -- Camp Security
+ServerModSetup("382177939")   -- DST Storm Cellar
+ServerModSetup("374550642")   -- Increased Stack Size
+ServerModSetup("1818688368")  -- Extra Equip Slots
+ServerModSetup("1839858501")  -- Large Boats
+ServerModSetup("2353205177")  -- Moving Box
+
+-- Cooperativo / Exploración
+ServerModSetup("378160973")   -- Global Positions
+
+-- Facilitan mecánicas (balanceados/configurables)
+ServerModSetup("2340409563")  -- Let Me Sprint!
+ServerModSetup("817025783")   -- Craftable Gears - Configurable!
